019e9ea74576edfc898ee14eb586e632d3c80034
gauravbrills <gauravbrills14@gmail.com>
2016-02-22 19:03:49 +0530

    excluded field class

--- src/main/java/me/gauravbrills/accesscontrol/ExcludedField.java
+++ src/main/java/me/gauravbrills/accesscontrol/ExcludedField.java
@@ -5,12 +5,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @Entity
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor
 public class ExcludedField {
 	private @Id @GeneratedValue Long id;
 	private @NonNull String fieldName;
